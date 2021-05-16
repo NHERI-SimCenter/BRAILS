299130d02add806d1dcd03a3add34f6d22267ae9
Charles Wang <c_w@berkeley.edu>
2021-05-16 14:01:40 -0700

    adding image downloading utils

--- brails/workflow/Images.py
+++ brails/workflow/Images.py
@@ -27,16 +27,18 @@ def capturePic(browser, picname):
 def download(urls):
     xcount = 0
     nlimit = 1e10
-    reDownload = urls[0][6]
+    reDownload = urls[0][5]
 
     for ls in urls:
         urlTop = ls[0]
         urlStreet = ls[1]
-        lon = ls[2]
-        lat = ls[3]
-        cats = ls[4]
-        imgDir = ls[5]
-        #reDownload = ls[6]
+        #lon = ls[2]
+        #lat = ls[3]
+        addr = ls[2]
+
+        cats = ls[3]
+        imgDir = ls[4]
+        #reDownload = ls[5]
 
         '''
         if rooftype not in roofcat:
@@ -53,8 +55,14 @@ def download(urls):
             for cat in cats:
                 if cat == 'StreetView': trueURL = urlStreet
                 elif cat == 'TopView': trueURL = urlTop
-                #picname = thisFileDir + '/{prefix}x{lon}x{lat}.png'.format(prefix='StreetView',lon=lon,lat=lat)
-                picname = Path(f'{imgDir}/{cat}/{cat}x{lon}x{lat}.png')
+
+                if type(addr) == str:
+                    addrstr = addr.replace(' ','-')
+                    picname = Path(f'{imgDir}/{cat}/{cat}x{addrstr}.png')
+                else:
+                    lon, lat = '%.6f'%addr[0], '%.6f'%addr[1]
+                    #picname = thisFileDir + '/{prefix}x{lon}x{lat}.png'.format(prefix='StreetView',lon=lon,lat=lat)
+                    picname = Path(f'{imgDir}/{cat}/{cat}x{lon}x{lat}.png')
 
                 exist = os.path.exists(picname)
                 if not exist or (exist and reDownload):
@@ -67,7 +75,7 @@ def download(urls):
 
                     if os.path.getsize(picname)/1024 < 9: 
                         #print(urlStreet)
-                        print(f"empty image from API: {lon}, {lat}")
+                        print(f"empty image from API: ", addr)
                         #exit() # empty image from API
 
         else:
@@ -128,4 +136,62 @@ def getGoogleImages(footprints=None, GoogleMapAPIKey='',imageTypes=['StreetView'
     pool.join()
     print('Images downloaded ...')
 
+def getGoogleImagesByAddrOrCoord(Addrs=None, GoogleMapAPIKey='',imageTypes=['StreetView','TopView'],imgDir='',ncpu=2,fov=60,pitch=0,reDownloadImgs=False):
 
+    if Addrs is None:
+        print('Please provide Addrs') 
+        return None
+
+    if GoogleMapAPIKey == '':
+        print('Please provide GoogleMapAPIKey') 
+        return None
+
+    for imgType in imageTypes:
+        tmpImgeDir = os.path.join(imgDir, imgType)
+        if not os.path.exists(tmpImgeDir): os.makedirs(tmpImgeDir)
+
+    
+    # APIs
+    baseurl_streetview_addr = "https://maps.googleapis.com/maps/api/streetview?size=640x640&location={addr}&fov={fov}&pitch={pitch}&source=outdoor&key="+GoogleMapAPIKey
+    baseurl_streetview_coord = "https://maps.googleapis.com/maps/api/streetview?size=640x640&location={lat},{lon}&fov={fov}&pitch={pitch}&source=outdoor&key="+GoogleMapAPIKey
+    # consider using 256x256 to save disk
+    
+    baseurl_satellite_addr="https://maps.googleapis.com/maps/api/staticmap?center={addr}&zoom=20&scale=1&size=256x256&maptype=satellite&key="+GoogleMapAPIKey+"&format=png&visual_refresh=true"
+    baseurl_satellite_coord="https://maps.googleapis.com/maps/api/staticmap?center={lat},{lon}&zoom=20&scale=1&size=256x256&maptype=satellite&key="+GoogleMapAPIKey+"&format=png&visual_refresh=true"
+
+
+    #footprints = gpd.read_file(BuildingFootPrintsFileName)
+    urls = []
+
+    for addr in Addrs:
+
+        if type(addr) == str:
+            urlTop = baseurl_satellite_addr.format(addr=addr)
+            urlStreet = baseurl_streetview_addr.format(addr=addr,fov=fov,pitch=pitch)
+        else:
+            lon, lat = '%.6f'%addr[0], '%.6f'%addr[1]
+            urlTop = baseurl_satellite_coord.format(lat=lat,lon=lon)
+            urlStreet = baseurl_streetview_coord.format(lat=lat,lon=lon,fov=fov,pitch=pitch)
+
+        cats = imageTypes
+        reDownload = 1 if reDownloadImgs else 0
+        urls.append([urlTop,urlStreet,addr,cats,imgDir,reDownload])
+
+    #print('shuffling...')
+    #random.shuffle(urls)
+    #print('shuffled...')     
+
+    # divide urls into small chunks
+    #ncpu = 4
+    step = int(len(urls)/ncpu)+1
+    chunks = [urls[x:x+step] for x in range(0, len(urls), step)]
+
+    print('Downloading images from Google API ...')
+    # get some workers
+    pool = ThreadPool(ncpu)
+    # send job to workers
+    results = pool.map(download, chunks)
+    # jobs are done, clean the site
+    pool.close()
+    pool.join()
+    print('Images downloaded ...')
