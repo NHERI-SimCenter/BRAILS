c6b0edd61fea4974ed72a8e4a8eeaba57025b356
Barbaros <bacetiner@ucla.edu>
2022-05-03 08:28:08 -0700

    Updated InventoryGenerator with the latest image and footprint handlers

--- brails/InventoryGenerator.py
+++ brails/InventoryGenerator.py
@@ -39,8 +39,8 @@
 # Last updated:
 # 04-25-2022 
 
-
-
+import random
+import sys
 import os
 from pathlib import Path
 import pandas as pd
@@ -49,61 +49,64 @@ from brails.modules import (PytorchRoofClassifier, PytorchOccupancyClassifier,
                             SoftstoryClassifier, FoundationHeightClassifier, 
                             YearBuiltClassifier, NFloorDetector, 
                             GarageDetector, ChimneyDetector)
-
-
-from .workflow.Footprints import getMSFootprintsByPlace, getStateNameByBBox, getOSMFootprints, getMSFootprintsByBbox
-from .workflow.Images import getGoogleImages
-
+from .workflow.ImageHandler import ImageHandler
+from .workflow.FootprintHandler import FootprintHandler
 
 class InventoryGenerator:
 
-    def __init__(self, location='', nbldgs=10, random=True, footprints='OSM',
-                 GoogleAPIKey=''):
-        def getFootprintByPlace(self,place='',workDir='',GoogleAPIKey='',save=True,overwrite=True):
-            if self.footPrints=='OSM':
-                print(f"Trying to get OSM footprints by the place name {place}.")
-                self.BIM, footprintFilename = getOSMFootprints(place=place,save=True,workDir=workDir,overwrite=True)
-            elif self.footPrints=='Microsoft':
-                #assert False, f"Trying to get Microsoft footprints by the place name {place}."
-                self.BIM, footprintFilename = getMSFootprintsByPlace(place=location,save=True,workDir='tmp',GoogleMapAPIKey=GoogleAPIKey,overwrite=False)
-            else: 
-                assert False, f"footPrints must be OSM or Microsoft in CityBuilder. Your value is {self.footPrints}."
-                
+    def __init__(self, location='', nbldgs=10, randomSelection=True,
+                 GoogleAPIKey=''):                
         self.workdir = 'tmp'
-        self.footprints = footprints
-        self.bimExist = False
+        self.location = location
+        self.nbldgs = nbldgs
+        self.randomSelection = randomSelection
+        self.apikey = GoogleAPIKey
         
-        if len(location)>0:
-            print(location)
-            bimout = Path(f'/{location}_BIM.geojson'.replace(' ','_').replace(',','_'))
-            bimFile = Path(f'tmp/{bimout}')
-            self.BIM = pd.read_file(self.bimFile)
-            self.bimExist = True
-            getFootprintByPlace(place=location,workDir='tmp',GoogleMapAPIKey=GoogleAPIKey,save=True,overwrite=True)
-        else:
-            assert False, f"footprint source must be OSM or Microsoft. Your value is {self.footPrints}."
-
+        # Get footprint data:
+        fpHandler = FootprintHandler()
+        fpHandler.load_footprint_data(self.location)
+        if self.randomSelection==True:
+            self.footprints = random.sample(fpHandler.footprints, nbldgs)
+        else: 
+            self.footprints = fpHandler.footprints[:nbldgs]
+            
+        # Initialize the image handler to check if the provided API key is valid:
+        image_handler = ImageHandler(self.apikey)
 
     def generate(self,attributes=['numstories','occupancy','roofshape']):
         self.attributes = attributes
-        
-        
+
+        # Download required images
+        image_handler = ImageHandler(self.apikey)
+        if 'roofshape' in attributes:
+            image_handler.GetGoogleSatelliteImage(self.footprints)
+            imsat = ['tmp/images/satellite/' + im for im in os.listdir('tmp/images/satellite')]
+        elif set.intersection(set(['chimney','elevated','garage','numstories',
+                                   'occupancy','year']),attributes)!=set():            
+            image_handler.GetGoogleStreetImage(self.footprints)
+            imstreet = ['tmp/images/street/' + im for im in os.listdir('tmp/images/street')]
+        else:
+            sys.exit('Defined list of attributes does not contain a correct' +
+                     'attribute entry. Attribute entries enabled are: chimney, ' + 
+                     'elevated, garage, numstories, occupancy, roofshape, year')
         
         for attribute in self.attributes:
             if attribute.lower()=='roofshape':
                 roofModel = PytorchRoofClassifier(modelName='transformer_rooftype_v1', download=True)
-                roofShape = roofModel.predictMultipleImages('images/satellite')
+                roofShape = roofModel.predictMultipleImages(imsat)
                 self.BIM['roofShape'] = self.BIM.apply(lambda x: roofShape[x['ID']], axis=1)
-            if attribute.lower()=='occupancy':
+
+            elif attribute.lower()=='occupancy':
                 occupancyModel = PytorchRoofClassifier(modelName='transformer_occupancy_v1', download=True)
-                occupancy = occupancyModel.predictMultipleImages('images/satellite')
+                occupancy = occupancyModel.predictMultipleImages(imstreet)
                 self.BIM['occupancy'] = self.BIM.apply(lambda x: occupancy[x['ID']], axis=1) 
+           
             elif attribute.lower()=='elevated':
                 # initialize a foundation classifier
                 elvModel = FoundationHeightClassifier(workDir=self.workDir,printRes=False)
 
                 # use the classifier 
-                elv_df = elvModel.predict(self.BIM['StreetView'].tolist())
+                elv_df = elvModel.predict(imstreet)
 
                 elv = elv_df['prediction'].to_list()
                 elvProb = elv_df['probability'].to_list()
@@ -115,7 +118,7 @@ class InventoryGenerator:
                 storyModel = NFloorDetector()
 
                 # Call the floor detector to determine number of floors
-                story_df = storyModel.predict(self.BIM['StreetView'].tolist())
+                story_df = storyModel.predict(imstreet)
 
                 # Write the results to a dataframe
                 story = story_df['prediction'].to_list()
@@ -125,28 +128,28 @@ class InventoryGenerator:
                 # Initialize the garage detector object
                 garageModel = GarageDetector()
 
-                # Call the floor detector to determine number of floors
-                garage_df = garageModel.predict(self.BIM['StreetView'].tolist())
+                # Call the garage detector to determine the existence of garages
+                garage_df = garageModel.predict(imstreet)
 
                 # Write the results to a dataframe
                 garage = garage_df['prediction'].to_list()
-                self.BIM['garageExist'] = self.BIM.apply(lambda x: story[x['ID']], axis=1)
+                self.BIM['garageExist'] = self.BIM.apply(lambda x: garage[x['ID']], axis=1)
 
             elif attribute.lower()=='chimney':
-                # Initialize the floor detector object
+                # Initialize the chimney detector object
                 chimneyModel = ChimneyDetector()
 
-                # Call the floor detector to determine number of floors
-                chimney_df = chimneyModel.predict(self.BIM['StreetView'].tolist())
+                # Call the chimney detector to existence of chimneys
+                chimney_df = chimneyModel.predict(imstreet)
 
                 # Write the results to a dataframe
                 chimney = chimney_df['prediction'].to_list()
-                self.BIM['chimneyExist'] = self.BIM.apply(lambda x: story[x['ID']], axis=1)
+                self.BIM['chimneyExist'] = self.BIM.apply(lambda x: chimney[x['ID']], axis=1)
                 
             elif attribute.lower()=='year':
                 yearModel = YearBuiltClassifier(workDir=self.workDir,printRes=False)
 
-                year_df = yearModel.predict(self.BIM['StreetView'].tolist())
+                year_df = yearModel.predict(imstreet)
 
                 year = year_df['prediction'].to_list()
                 yearProb = year_df['probability'].to_list()
