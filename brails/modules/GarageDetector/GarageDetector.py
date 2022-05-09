f4531cb426f7f848fac8fa8b1c00a6c0b0fec035
Barbaros <bacetiner@ucla.edu>
2022-05-08 18:59:43 -0700

    Total execution time for detector modules now print on a new line

--- brails/modules/GarageDetector/GarageDetector.py
+++ brails/modules/GarageDetector/GarageDetector.py
@@ -234,7 +234,7 @@ class GarageDetector():
         endTime = time.time()
         hours, rem = divmod(endTime-startTime, 3600)
         minutes, seconds = divmod(rem, 60)
-        print("Total execution time: {:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+        print("\nTotal execution time: {:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
         
         # Cleanup the Root Folder
         if os.path.isfile("input.jpg"):
