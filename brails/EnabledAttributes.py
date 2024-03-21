005abcfeef883651516b9c416d651d37a1c486db
bacetiner <bacetiner@ucla.edu>
2024-03-21 05:57:24 -0700

    Function to get list of attributes without initializing InventoryGenerator

---
+++ brails/EnabledAttributes.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def BldgAttributes():
+    attr = ['buildingheight','chimney','erabuilt',
+                              'garage','numstories','occupancy',
+                              'roofeaveheight','roofshape','roofpitch',
+                              'constype', 'roofcover','constype']
+    return attr
